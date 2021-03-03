599321813c4d64bc74501f9d266d343f39881a42
Melissa Celeste Gries <m.c.gries@gmx.de>
2021-03-03 12:09:42 +0100

    add card request

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const app = express() // damit bekommen wir eine Express-app - App ist hier eine Instanz - keine App wie wir sie können
 
-app.use((req, res) => {
-    console.log(req.method, req.url)
-    res.end('Hello World')
-})
+
+app.use('/api/users', (req, res) => {
+    res.end('[{"name": "Melissa", "role": "student"}]')
+  })
+  app.use('/api/cards', (req, res) => {
+    res.end('[{"title": "First card"}]')
+  })
+ 
+
+
+
+
+    // req.url === '/api/users'  // wenn url gleich '/api/users' ist
+//  ? res.end('[{"name": "Melissa", "role":"student"}]') //  dann schicke diese Daten zurück
+//  : next () // ansonsten geh zum nächsten Befehl weiter
+
+    // console.log(req.method, req.url)
+    // res.end('Hello World')
 
 app.listen(3000, ()=> { // unter welchem Port der Server gestartet werden soll
 console.log('Server started at locoalhost:3000')
