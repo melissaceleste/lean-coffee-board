57e42a2e8c667599676b41f3cdf7c1690f0c9ceb
Melissa Celeste Gries <m.c.gries@gmx.de>
2021-03-03 15:17:06 +0100

    express examples

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+uuidv4()
 const app = express() // damit bekommen wir eine Express-app - App ist hier eine Instanz - keine App wie wir sie können
+let users = []
 
+app.use(express.json())
 
-app.use('/api/users', (req, res) => {
-    res.end('[{"name": "Melissa", "role": "student"}]')
-  })
-  app.use('/api/cards', (req, res) => {
-    res.end('[{"title": "First card"}]')
-  })
- 
+app.get('/api/users', (req, res) => {
+  // hiermit lassen wir uns die /api/users anzeigen
+  res.json(users)
+})
 
+app.get('/api/users/:id', (req, res) => {
+  // hiermit lassen wir uns die jeweiligen User mit der entsprechenden ID (die wir einfügen) anzeigen lassen
+  const { id } = req.params
+  res.json(users.find(user => user.id === id))
+})
 
+app.post('/api/users', (req, res) => {
+  // hiermit können wir neue User hinzufügen
+  const newUser = { ...req.body, id: uuidv4() }
+  users.push(newUser)
+  res.json(newUser)
+})
 
+app.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params
+  const index = users.findIndex(user => user.id === id)
+  users = [...users.slice(0, index), ...users.slice(index + 1)]
+  res.json(users)
+})
 
-    // req.url === '/api/users'  // wenn url gleich '/api/users' ist
-//  ? res.end('[{"name": "Melissa", "role":"student"}]') //  dann schicke diese Daten zurück
-//  : next () // ansonsten geh zum nächsten Befehl weiter
-
-    // console.log(req.method, req.url)
-    // res.end('Hello World')
-
-app.listen(3000, ()=> { // unter welchem Port der Server gestartet werden soll
-console.log('Server started at locoalhost:3000')
+app.listen(3000, () => {
+  console.log('Server started at locoalhost:3000')
 })
